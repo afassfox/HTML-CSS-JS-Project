cfd0edb2e0b639d52c42b131c3df2e01813b1e90
afassfox <159053546+afassfox@users.noreply.github.com>
2025-04-28 17:18:37 +0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -4,80 +4,78 @@ let toggleMenu = document.querySelector(".header-area .menu-icon");
 let toggleMenuLinks = document.querySelector(".links");
 
 toggleMenuIcon.onclick = function (e) {
-    e.stopPropagation();
-    // Add "active-menu" Class for arrow
-    toggleMenu.classList.toggle("active-menu");
-    // Add "open" Class for Links
-    toggleMenuLinks.classList.toggle("open");
+  e.stopPropagation();
+  // Add "active-menu" Class for arrow
+  toggleMenu.classList.toggle("active-menu");
+  // Add "open" Class for Links
+  toggleMenuLinks.classList.toggle("open");
 };
 
 document.addEventListener("click", (e) => {
-    if (e.target !== toggleMenuIcon && e.target !== toggleMenuLinks) {
-        if (toggleMenuLinks.classList.contains("open")) {
-            toggleMenu.classList.remove("active-menu");
-            toggleMenuLinks.classList.remove("open");
-        }
+  if (e.target !== toggleMenuIcon && e.target !== toggleMenuLinks) {
+    if (toggleMenuLinks.classList.contains("open")) {
+      toggleMenu.classList.remove("active-menu");
+      toggleMenuLinks.classList.remove("open");
     }
+  }
 });
 
 toggleMenuLinks.onclick = function (e) {
-    e.stopPropagation();
+  e.stopPropagation();
 };
 
 // Check local storage color option
 let mainColors = localStorage.getItem("color_option");
 
 if (mainColors !== null) {
-    document.documentElement.style.setProperty("--main-color", mainColors);
+  document.documentElement.style.setProperty("--main-color", mainColors);
 
-    //check for active class
-    document.querySelectorAll(".colors-list li").forEach((element) => {
-        element.classList.remove("active");
-        if (element.dataset.color === mainColors) {
-            element.classList.add("active");
-        }
-    });
+  //check for active class
+  document.querySelectorAll(".colors-list li").forEach((element) => {
+    element.classList.remove("active");
+    if (element.dataset.color === mainColors) {
+      element.classList.add("active");
+    }
+  });
 }
 // Select Gear Button
 let toggleGear = document.querySelector(".toggle-settings .fa-gear");
 
 // Make Gear spin on hover
 toggleGear.addEventListener("mouseover", () => {
-    toggleGear.classList.toggle("fa-spin");
+  toggleGear.classList.toggle("fa-spin");
 });
 toggleGear.addEventListener("mouseout", () => {
-    toggleGear.classList.toggle("fa-spin");
+  toggleGear.classList.toggle("fa-spin");
 });
 
 toggleGear.onclick = function () {
-    // Toggle Open On Main Settings Box
-    document.querySelector(".settings-box").classList.toggle("open");
+  // Toggle Open On Main Settings Box
+  document.querySelector(".settings-box").classList.toggle("open");
 };
 
 //Switch Colors
 const colorsLi = document.querySelectorAll(".colors-list li");
 
 colorsLi.forEach((li) => {
-    li.addEventListener("click", (e) => {
-        //Set color on root
-        document.documentElement.style.setProperty(
-            "--main-color",
-            e.target.dataset.color
-        );
-        //Set color on local storage
-        localStorage.setItem("color_option", e.target.dataset.color);
-        //remove active class from all childern
-        e.target.parentElement
-            .querySelectorAll(".active")
-            .forEach((element) => {
-                element.classList.remove("active");
-            });
-        // add active class
-        e.target.classList.add("active");
+  li.addEventListener("click", (e) => {
+    //Set color on root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+    //Set color on local storage
+    localStorage.setItem("color_option", e.target.dataset.color);
+    //remove active class from all childern
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
     });
+    // add active class
+    e.target.classList.add("active");
+  });
 });
 
-// Handle bullets active class on scroll
+// Select bullets
 const allBullets = document.querySelectorAll(".nav-bullets .bullet");
 const sections = document.querySelectorAll(".section");
 
@@ -102,15 +100,15 @@ const allLinks = document.querySelectorAll(".links a");
 
 // Scrolling Function
 function scrollToSection(elements) {
-    elements.forEach((element) => {
-        element.addEventListener("click", (e) => {
-            e.preventDefault();
+  elements.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
 
-            document
-                .querySelector(e.target.dataset.section)
-                .scrollIntoView({ behavior: "smooth", block: "center" });
-        });
+      document
+        .querySelector(e.target.dataset.section)
+        .scrollIntoView({ behavior: "smooth", block: "center" });
     });
+  });
 }
 scrollToSection(allBullets);
 scrollToSection(allLinks);
@@ -120,30 +118,30 @@ let savedNavBulletsOption = localStorage.getItem("bullets_option");
 let saveBullets = document.querySelector("#bullets");
 
 if (savedNavBulletsOption !== null) {
-    if (savedNavBulletsOption === "block") {
-        saveBullets.checked = true;
-    } else {
-        saveBullets.checked = false;
-    }
-    // Set Checked attribute from checkbox based on savedNavBulletsOption value in local storage
-    if (savedNavBulletsOption === "block") {
-        document.querySelector(".nav-bullets").style.display = "block";
-    } else {
-        document.querySelector(".nav-bullets").style.display = "none";
-    }
+  if (savedNavBulletsOption === "block") {
+    saveBullets.checked = true;
+  } else {
+    saveBullets.checked = false;
+  }
+  // Set Checked attribute from checkbox based on savedNavBulletsOption value in local storage
+  if (savedNavBulletsOption === "block") {
+    document.querySelector(".nav-bullets").style.display = "block";
+  } else {
+    document.querySelector(".nav-bullets").style.display = "none";
+  }
 }
 // Toggle Nav Bullets
 const navBullets = document
-    .querySelector("#bullets")
-    .addEventListener("change", (e) => {
-        if (e.target.checked) {
-            document.querySelector(".nav-bullets").style.display = "block";
-            localStorage.setItem("bullets_option", "block");
-        } else {
-            document.querySelector(".nav-bullets").style.display = "none";
-            localStorage.setItem("bullets_option", "none");
-        }
-    });
+  .querySelector("#bullets")
+  .addEventListener("change", (e) => {
+    if (e.target.checked) {
+      document.querySelector(".nav-bullets").style.display = "block";
+      localStorage.setItem("bullets_option", "block");
+    } else {
+      document.querySelector(".nav-bullets").style.display = "none";
+      localStorage.setItem("bullets_option", "none");
+    }
+  });
 
 //random background option
 let backgroundOption = true;
@@ -156,17 +154,17 @@ let savedBackgroundOption = localStorage.getItem("background_option");
 let saveButton = document.querySelector("#background");
 
 if (savedBackgroundOption !== null) {
-    if (savedBackgroundOption === "true") {
-        backgroundOption = true;
-    } else {
-        backgroundOption = false;
-    }
-    // Add/Remove Checked attribute from checkbox based on savedBackgroundOption value in local storage
-    if (savedBackgroundOption === "true") {
-        saveButton.checked = true;
-    } else {
-        saveButton.checked = false;
-    }
+  if (savedBackgroundOption === "true") {
+    backgroundOption = true;
+  } else {
+    backgroundOption = false;
+  }
+  // Add/Remove Checked attribute from checkbox based on savedBackgroundOption value in local storage
+  if (savedBackgroundOption === "true") {
+    saveButton.checked = true;
+  } else {
+    saveButton.checked = false;
+  }
 }
 
 // Select Landing Page Element
@@ -180,28 +178,28 @@ landingPageElement.style.backgroundImage = 'url("imgs/img1.jpg")';
 
 // Toggle random background option
 const randomBackground = document
-    .querySelector("#background")
-    .addEventListener("change", (e) => {
-        if (e.target.checked) {
-            backgroundOption = true;
-            randomizeImg();
-            localStorage.setItem("background_option", true);
-        } else {
-            backgroundOption = false;
-            clearInterval(backgroundInterval);
-            localStorage.setItem("background_option", false);
-        }
-    });
+  .querySelector("#background")
+  .addEventListener("change", (e) => {
+    if (e.target.checked) {
+      backgroundOption = true;
+      randomizeImg();
+      localStorage.setItem("background_option", true);
+    } else {
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+      localStorage.setItem("background_option", false);
+    }
+  });
 
 function randomizeImg() {
-    if (backgroundOption) {
-        backgroundInterval = setInterval(() => {
-            //Get Random Number
-            let randomNumber = Math.floor(Math.random() * imgArray.length);
+  if (backgroundOption) {
+    backgroundInterval = setInterval(() => {
+      //Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgArray.length);
 
-            landingPageElement.style.backgroundImage = `url("imgs/${imgArray[randomNumber]}")`;
-        }, 5000);
-    }
+      landingPageElement.style.backgroundImage = `url("imgs/${imgArray[randomNumber]}")`;
+    }, 5000);
+  }
 }
 randomizeImg();
 
@@ -209,135 +207,135 @@ randomizeImg();
 let ourSkills = document.querySelector(".skills");
 
 window.onscroll = function () {
-    // Skills offset Top
-    let skillsOffsetTop = ourSkills.offsetTop;
-    // console.log(skillsOffsetTop);
+  // Skills offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+  // console.log(skillsOffsetTop);
 
-    // Skills Outer Height
-    let skillsOuterHeight = ourSkills.offsetHeight;
-    // console.log(skillsOuterHeight);
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+  // console.log(skillsOuterHeight);
 
-    // Window Height
-    let windowHeight = this.innerHeight;
-    // console.log(windowHeight);
+  // Window Height
+  let windowHeight = this.innerHeight;
+  // console.log(windowHeight);
 
-    //window Scroll Top
-    let windowScrollTop = this.pageYOffset;
-    // console.log(windowScrollTop);
+  //window Scroll Top
+  let windowScrollTop = this.pageYOffset;
+  // console.log(windowScrollTop);
 
-    if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
-        let allSkills = document.querySelectorAll(
-            ".skill-box .skill-progress span"
-        );
-        allSkills.forEach((skill) => {
-            skill.style.width = skill.dataset.progress;
-        });
-    }
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
 };
 
 //create popup for images
 let ourGallery = document.querySelectorAll(".gallery img");
 
 ourGallery.forEach((img) => {
-    img.addEventListener("click", (e) => {
-        //create overlay element
-        let overlay = document.createElement("div");
+  img.addEventListener("click", (e) => {
+    //create overlay element
+    let overlay = document.createElement("div");
 
-        //overlay class
-        overlay.className = "popup-overlay";
+    //overlay class
+    overlay.className = "popup-overlay";
 
-        //append overlay to body
-        document.body.appendChild(overlay);
+    //append overlay to body
+    document.body.appendChild(overlay);
 
-        //create the popup
-        let popupBox = document.createElement("div");
+    //create the popup
+    let popupBox = document.createElement("div");
 
-        // popup class
-        popupBox.className = "popup-box";
+    // popup class
+    popupBox.className = "popup-box";
 
-        // add alt text
-        if (img.alt !== null) {
-            // creating heading for alt
-            let imgHeading = document.createElement("h3");
+    // add alt text
+    if (img.alt !== null) {
+      // creating heading for alt
+      let imgHeading = document.createElement("h3");
 
-            //create text for heading
-            let imgText = document.createTextNode(img.alt);
+      //create text for heading
+      let imgText = document.createTextNode(img.alt);
 
-            //append text to heading
-            imgHeading.appendChild(imgText);
+      //append text to heading
+      imgHeading.appendChild(imgText);
 
-            // append heading to popup box
-            popupBox.appendChild(imgHeading);
-        }
+      // append heading to popup box
+      popupBox.appendChild(imgHeading);
+    }
 
-        // create the image
-        let popupImage = document.createElement("img");
+    // create the image
+    let popupImage = document.createElement("img");
 
-        // set image src
-        popupImage.src = img.src;
+    // set image src
+    popupImage.src = img.src;
 
-        // add image to popup box
-        popupBox.appendChild(popupImage);
+    // add image to popup box
+    popupBox.appendChild(popupImage);
 
-        // append popup to body
-        document.body.appendChild(popupBox);
+    // append popup to body
+    document.body.appendChild(popupBox);
 
-        // create close span
-        let closeButton = document.createElement("span");
+    // create close span
+    let closeButton = document.createElement("span");
 
-        // create close button X
-        let closeButtonX = document.createTextNode("X");
+    // create close button X
+    let closeButtonX = document.createTextNode("X");
 
-        // append X to Close button
-        closeButton.appendChild(closeButtonX);
+    // append X to Close button
+    closeButton.appendChild(closeButtonX);
 
-        // class for close button
-        closeButton.className = "close-button";
+    // class for close button
+    closeButton.className = "close-button";
 
-        //add close button to popup box
-        popupBox.appendChild(closeButton);
-    });
+    //add close button to popup box
+    popupBox.appendChild(closeButton);
+  });
 });
 
 // close popup by clicking on X
 document.addEventListener("click", (e) => {
-    if (e.target.className === "close-button") {
-        // remove popup
-        e.target.parentNode.remove();
+  if (e.target.className === "close-button") {
+    // remove popup
+    e.target.parentNode.remove();
 
-        // remove overlay
-        document.querySelector(".popup-overlay").remove();
-    }
+    // remove overlay
+    document.querySelector(".popup-overlay").remove();
+  }
 });
 
 // close popup by clicking anywhere
 document.addEventListener("click", (e) => {
-    if (e.target.className === "popup-overlay") {
-        document.querySelector(".popup-box").remove();
-        document.querySelector(".popup-overlay").remove();
-    }
+  if (e.target.className === "popup-overlay") {
+    document.querySelector(".popup-box").remove();
+    document.querySelector(".popup-overlay").remove();
+  }
 });
 
 // Reset Button
 document.querySelector(".reset-settings").onclick = () => {
-    localStorage.removeItem("bullets_option");
-    localStorage.removeItem("color_option");
-    localStorage.removeItem("background_option");
+  localStorage.removeItem("bullets_option");
+  localStorage.removeItem("color_option");
+  localStorage.removeItem("background_option");
 
-    window.location.reload();
+  window.location.reload();
 };
 
 // Timeline scrolling
 const timelineScroll = new IntersectionObserver(
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-            }
-        });
-    },
-    { threshold: 0.5 }
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.5 }
 );
 document.querySelectorAll(".hidden").forEach((element) => {
-    timelineScroll.observe(element);
+  timelineScroll.observe(element);
 });
